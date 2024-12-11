9451eb47528ded425379237c2c7524416c75c61c
Kristofer Kerren <kristofer.kerren@crowdcollective.com>
2024-12-11 08:36:28 +0100

    Day11b

--- 11/a.ts
+++ 11/a.ts
@@ -1,40 +1,54 @@
-class Coord {
-  constructor(public x: number, public y: number) {}
+class Stones {
+  constructor(public val: number, public amount: number = 1) {}
 }
 
 const fs = require('fs');
-let stones: number[] = fs
+let stones: Stones[] = fs
   .readFileSync('11/input.txt', 'utf8')
   .split(' ')
-  .map(Number);
-const cache: Map<number, number> = new Map();
+  .map((v) => new Stones(Number(v)));
+//const cache: Map<number, number> = new Map();
 
-const transform = (stone: number): number[] => {
-  if (stone === 0) return [1];
-  const nbrOfDigits = `${stone}`.length;
+const transform = (stoneVal: number): number[] => {
+  if (stoneVal === 0) return [1];
+  const nbrOfDigits = `${stoneVal}`.length;
   if (nbrOfDigits % 2 === 0)
     return [
-      Number(`${stone}`.substring(0, nbrOfDigits / 2)),
-      Number(`${stone}`.substring(nbrOfDigits / 2)),
+      Number(`${stoneVal}`.substring(0, nbrOfDigits / 2)),
+      Number(`${stoneVal}`.substring(nbrOfDigits / 2)),
     ];
-  return [stone * 2024];
+  return [stoneVal * 2024];
 };
 
-const transformMultiple = (stones: number[]): number[] => {
-  const newStones: number[] = [];
-  stones.forEach((stone) => newStones.push(...transform(stone)));
+const transformMultiple = (stones: Stones[]): Stones[] => {
+  const newStones: Stones[] = [];
+  stones.forEach((stone) =>
+    transform(stone.val).forEach((v) => {
+      const existing = newStones.find((s) => s.val === v);
+      if (existing) existing.amount += stone.amount;
+      else newStones.push(new Stones(v, stone.amount));
+    })
+  );
   return newStones;
 };
 
-const getNbrOfSteps = (stones: number[], steps: number): number => {
+const getNbrOfSteps = (stones: Stones[], steps: number) => {
   let i = 0;
   while (i < steps) {
     stones = transformMultiple(stones);
     i++;
+    if (i === 25) printNbrOfSteps(stones, 'Day 11-1');
+    if (i === 75) printNbrOfSteps(stones, 'Day 11-2');
   }
-  return stones.length;
 };
 
-console.log({ nbrOfSteps: getNbrOfSteps(stones, 25) });
+const printNbrOfSteps = (stones: Stones[], description: string): void => {
+  console.log({
+    description,
+    nbrOfSteps: stones.map((s) => s.amount).reduce((a, b) => a + b, 0),
+  });
+};
+
+getNbrOfSteps(stones, 75);
 
 export {};
