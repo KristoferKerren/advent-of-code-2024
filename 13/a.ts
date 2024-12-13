bec88ab47e94555f2387ed9f609030dca7013882
Kristofer Kerren <kristofer.kerren@crowdcollective.com>
2024-12-13 08:55:02 +0100

    Refactor day 13

--- 13/a.ts
+++ 13/a.ts
@@ -2,7 +2,7 @@ class Button {
   constructor(
     public deltaX: number,
     public deltaY: number,
-    public cost: number
+    public cost: number = 0
   ) {}
 }
 
@@ -14,25 +14,27 @@ class ClawMachine {
     public PrizeY: number = 0
   ) {}
 
-  getIsSolved(buttonAPress: number, buttonBPress: number): boolean {
-    return (
-      buttonAPress * this.buttonA.deltaX +
-        buttonBPress * this.buttonB.deltaX ===
-        this.PrizeX &&
-      buttonAPress * this.buttonA.deltaY +
-        buttonBPress * this.buttonB.deltaY ===
-        this.PrizeY
-    );
-  }
-
   getTotalCost(buttonAPress: number, buttonBPress: number): number {
     return buttonAPress * this.buttonA.cost + buttonBPress * this.buttonB.cost;
+  }
+
+  get buttonPresses() {
+    var buttonA =
+      (this.PrizeY * this.buttonB.deltaX - this.PrizeX * this.buttonB.deltaY) /
+      (this.buttonB.deltaX * this.buttonA.deltaY -
+        this.buttonA.deltaX * this.buttonB.deltaY);
+    var buttonB =
+      (this.PrizeX - this.buttonA.deltaX * buttonA) / this.buttonB.deltaX;
+    if (buttonA % 1 === 0 && buttonB % 1 === 0) {
+      return this.getTotalCost(buttonA, buttonB);
+    }
+    return 0;
   }
 }
 
 const fs = require('fs');
 
-function getClawMachines(fileName: string) {
+function getClawMachines(fileName: string, deltaPrice: number = 0) {
   const clawMachines: ClawMachine[] = [];
   const data: string = fs.readFileSync(fileName, 'utf8');
   data.split(/\r?\n/).forEach((row, x) => {
@@ -45,8 +47,8 @@ function getClawMachines(fileName: string) {
         clawMachines.at(-1).buttonB = new Button(deltaX, deltaY, 1);
       } else if (row.startsWith('Prize:')) {
         const [PrizeX, PrizeY] = row.substring(9).split(', Y=').map(Number);
-        clawMachines.at(-1).PrizeX = PrizeX;
-        clawMachines.at(-1).PrizeY = PrizeY;
+        clawMachines.at(-1).PrizeX = deltaPrice + PrizeX;
+        clawMachines.at(-1).PrizeY = deltaPrice + PrizeY;
       }
     }
   });
@@ -54,26 +56,18 @@ function getClawMachines(fileName: string) {
   return clawMachines;
 }
 
-const getCheapeastPrice = (clawMachine: ClawMachine) => {
-  for (var buttonAPress = 1; buttonAPress < 100; buttonAPress++) {
-    for (var buttonBPress = 1; buttonBPress < 100; buttonBPress++) {
-      if (clawMachine.getIsSolved(buttonAPress, buttonBPress)) {
-        return clawMachine.getTotalCost(buttonAPress, buttonBPress);
-      }
-    }
-  }
-  return 0;
-};
+const clawMachines13a = getClawMachines('13/input.txt');
+let sum13a = clawMachines13a.reduce(
+  (acc, clawMachine) => acc + clawMachine.buttonPresses,
+  0
+);
 
-const clawMachines = getClawMachines('13/input.txt');
+const clawMachines13b = getClawMachines('13/input.txt', 10000000000000);
+let sum13b = clawMachines13b.reduce(
+  (acc, clawMachine) => acc + clawMachine.buttonPresses,
+  0
+);
 
-console.log(clawMachines[0]);
-let sum = 0;
-clawMachines.forEach((clawMachine) => {
-  const cost = getCheapeastPrice(clawMachine);
-  sum += cost;
-});
-
-console.log({ sum });
+console.log({ sum13a, sum13b });
 
 export {};
