0e82f43506efcfaa970890adc7c17662ffaa8a92
Kristofer Kerren <kristofer.kerren@crowdcollective.com>
2024-12-17 10:12:34 +0100

    Day 17a

---
+++ 17/a.ts
@@ -0,0 +1,113 @@
+namespace AdventOfCode17 {
+  export class Program {
+    constructor(
+      public registerA: number,
+      public registerB: number,
+      public registerC: number,
+      public instructions: number[] = []
+    ) {}
+
+    isEqual(p: Program) {
+      return (
+        this.registerA === p.registerA &&
+        this.registerB === p.registerB &&
+        this.registerC === p.registerC
+      );
+    }
+  }
+
+  function nbrToBinary(n: number): string {
+    return n.toString(2);
+  }
+
+  function binaryToNbr(bin: string): number {
+    return parseInt(bin, 2);
+  }
+
+  function getDivision(n1: number, n2: number) {
+    const numerator = n1;
+    const denominator = Math.pow(2, n2);
+    return Math.trunc(numerator / denominator);
+  }
+
+  function getMod8(n: number): number {
+    let binary = nbrToBinary(n);
+    let last3 = binary.slice(-3);
+    return binaryToNbr(last3);
+  }
+
+  function getBitwiseXOR(n1: number, n2: number) {
+    let bin1 = nbrToBinary(n1);
+    let bin2 = nbrToBinary(n2);
+    if (bin1.length > bin2.length) {
+      bin2 = `${'0'.repeat(bin1.length - bin2.length)}${bin2}`;
+    } else if (bin2.length > bin1.length) {
+      bin1 = `${'0'.repeat(bin2.length - bin1.length)}${bin1}`;
+    }
+    let result = '';
+    for (let i = 0; i < bin1.length; i++) {
+      result += bin1[i] === bin2[i] ? '0' : '1';
+    }
+    return binaryToNbr(result);
+  }
+
+  export function runProgram(program: Program): number[] {
+    let index = 0;
+    let output: number[] = [];
+    while (index < program.instructions.length) {
+      const opcode = program.instructions[index];
+      const literalOperand = program.instructions[index + 1];
+      const comboOperand =
+        literalOperand === 4
+          ? program.registerA
+          : literalOperand === 5
+          ? program.registerB
+          : literalOperand === 6
+          ? program.registerC
+          : literalOperand;
+
+      switch (opcode) {
+        case 0:
+          program.registerA = getDivision(program.registerA, comboOperand);
+          break;
+        case 1:
+          program.registerB = getBitwiseXOR(program.registerB, literalOperand);
+          break;
+        case 2:
+          program.registerB = getMod8(comboOperand);
+          break;
+        case 3:
+          if (program.registerA !== 0) {
+            index = literalOperand - 2; //-2 to adjust for adding +2 after switch case
+          }
+          break;
+        case 4:
+          program.registerB = getBitwiseXOR(
+            program.registerB,
+            program.registerC
+          );
+          break;
+        case 5:
+          const mod8 = getMod8(comboOperand);
+          output = [...output, ...`${mod8}`.split('').map(Number)];
+          break;
+        case 6:
+          program.registerB = getDivision(program.registerA, comboOperand);
+          break;
+        case 7:
+          program.registerC = getDivision(program.registerA, comboOperand);
+          break;
+        default:
+          console.error('N0t implemented');
+      }
+      index += 2;
+    }
+    return output;
+  }
+
+  const instructions = [2, 4, 1, 1, 7, 5, 4, 7, 1, 4, 0, 3, 5, 5, 3, 0];
+  const program = new Program(30553366, 0, 0, instructions);
+  console.log(runProgram(program).join(','));
+}
+
+export { AdventOfCode17 };
---
+++ 17/tests.test.ts
@@ -0,0 +1,41 @@
+const { AdventOfCode17 } = require('./a');
+
+test('Intcode runProgram 1', () => {
+  const program = new AdventOfCode17.Program(0, 0, 9, [2, 6]);
+  console.log({ program });
+  const expectedFinalProgram = new AdventOfCode17.Program(0, 1, 9);
+  AdventOfCode17.runProgram(program);
+  expect(program.isEqual(expectedFinalProgram)).toBe(true);
+});
+
+test('Intcode runProgram 2', () => {
+  const program = new AdventOfCode17.Program(10, 0, 0, [5, 0, 5, 1, 5, 4]);
+  const output = AdventOfCode17.runProgram(program);
+  expect(output.join(',')).toBe('0,1,2');
+});
+
+test('Intcode runProgram 3', () => {
+  const program = new AdventOfCode17.Program(2024, 0, 9, [0, 1, 5, 4, 3, 0]);
+  const output = AdventOfCode17.runProgram(program);
+  expect(output.join(',')).toBe('4,2,5,6,7,7,7,7,3,1,0');
+  expect(program.registerA).toBe(0);
+});
+
+test('Intcode runProgram 4', () => {
+  const program = new AdventOfCode17.Program(0, 29, 0, [1, 7]);
+  const expectedFinalProgram = new AdventOfCode17.Program(0, 26, 0);
+  AdventOfCode17.runProgram(program);
+  expect(program.isEqual(expectedFinalProgram)).toBe(true);
+});
+
+test('Intcode runProgram 5', () => {
+  const program = new AdventOfCode17.Program(0, 2024, 43690, [4, 0]);
+  AdventOfCode17.runProgram(program);
+  expect(program.registerB).toBe(44354);
+});
+
+test('Intcode runProgram 6', () => {
+  const program = new AdventOfCode17.Program(729, 0, 0, [0, 1, 5, 4, 3, 0]);
+  const output = AdventOfCode17.runProgram(program);
+  expect(output.join(',')).toBe('4,6,3,5,6,3,5,2,1,0');
+});
